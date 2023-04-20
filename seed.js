8092804c713bfaf553290ddbc071fbbd07a252ac
dominicf2001 <dominicf2001@gmail.com>
2023-04-20 13:12:37 -0400

    Add seed.js file and do some test code

---
+++ seed.js
@@ -0,0 +1,13 @@
+import Database from 'better-sqlite3';
+const db = new Database('library.db');
+
+db.prepare('drop table if exists test').run();
+db.prepare('create table test (message varchar(20), numb int)').run();
+const testString = "Hello world";
+const testNum = 5;
+db.prepare('insert into test values(?, ?)').run(testString, testNum);
+db.prepare('insert into test values(?, 5)').run("Goodbye world");
+
+const row = db.prepare('select * from test').get();
+console.log(row);
+
