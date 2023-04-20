4dcf17825b841da2456cf1611b09f879e1cf94bc
dominicf2001 <dominicf2001@gmail.com>
2023-04-20 14:29:34 -0400

    Install browser-sync, update home.html to index.html and update the links to work with it

--- books.js
+++ books.js
@@ -4,28 +4,36 @@ import loggedInUser from "./globals.js"
 
 const bookGrid = document.querySelector(".book-grid");
 
-const bookItem = document.createElement("li");
-bookItem.classList.add("book-grid-item");
+function addBook(title, src) {
+    const bookItem = document.createElement("li");
+    bookItem.classList.add("book-grid-item");
 
-const bookContent = document.createElement("div");
-bookContent.classList.add("book-content");
+    const bookContent = document.createElement("div");
+    bookContent.classList.add("book-content");
 
-const bookImage = document.createElement("img");
-bookImage.setAttribute("src", "images/book1.png");
-bookContent.appendChild(bookImage);
+    const bookImage = document.createElement("img");
+    bookImage.setAttribute("src", `images/${src}`);
+    bookContent.appendChild(bookImage);
 
-const bookLinkTitle = document.createElement("a");
-bookLinkTitle.textContent = "Book title";
-bookContent.appendChild(bookLinkTitle);
+    const bookLinkTitle = document.createElement("a");
+    bookLinkTitle.textContent = title;
+    bookContent.appendChild(bookLinkTitle);
 
-const checkoutButton = document.createElement("button");
-checkoutButton.textContent = "Checkout"
-const infoButton = document.createElement("button");
-infoButton.textContent = "More Info"
-const buttonContainer = document.createElement("div");
-buttonContainer.appendChild(checkoutButton);
-buttonContainer.appendChild(infoButton);
-bookContent.appendChild(buttonContainer);
+    const checkoutButton = document.createElement("button");
+    checkoutButton.textContent = "Checkout"
+    const infoButton = document.createElement("button");
+    infoButton.textContent = "More Info"
+    const buttonContainer = document.createElement("div");
+    buttonContainer.appendChild(checkoutButton);
+    buttonContainer.appendChild(infoButton);
+    bookContent.appendChild(buttonContainer);
 
-bookItem.appendChild(bookContent);
-bookGrid.appendChild(bookItem);
+    bookItem.appendChild(bookContent);
+    bookGrid.appendChild(bookItem);
+}
+
+
+addBook("Works on My Machine", "book1.png");
+addBook("Works on My Machine", "book2.png");
+addBook("Works on My Machine", "book3.jpg");
+addBook("Works on My Machine", "book4.jpg");
