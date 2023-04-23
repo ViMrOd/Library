caad93d77b9e423762e9e0ac7d147dcea41a4518
Dominic Ferrando <dominicf2001@gmail.com>
2023-04-23 11:00:27 -0400

    Fix database import and globals.js

--- globals.js
+++ globals.js
@@ -1,9 +1,6 @@
-import Database from 'better-sqlite3';
-
 const loggedInUser = {
     username: "",
     isAdmin: false
 }
-const database = new Database('library.db')
 
-export { loggedInUser, database }
+export { loggedInUser };
--- seed.js
+++ seed.js
@@ -1,12 +1,12 @@
-import { database } from "./globals.js"
+const db = require('better-sqlite3')('library.db');
 
-database.prepare('drop table if exists test').run();
-database.prepare('create table test (message varchar(20), numb int)').run();
+db.prepare('drop table if exists test').run();
+db.prepare('create table test (message varchar(20), numb int)').run();
 const testString = "Hello world";
 const testNum = 5;
-database.prepare('insert into test values(?, ?)').run(testString, testNum);
-database.prepare('insert into test values(?, 5)').run("Goodbye world");
+db.prepare('insert into test values(?, ?)').run(testString, testNum);
+db.prepare('insert into test values(?, 5)').run("Goodbye world");
 
-const row = database.prepare('select * from test').get();
+const row = db.prepare('select * from test').get();
 console.log(row);
 
