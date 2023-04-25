db1db7e7c56c73027424b8cf1045bc915d756f33
Dominic Ferrando <dominicf2001@gmail.com>
2023-04-24 22:48:07 -0400

    Make login and sign out system function

--- utility/header-login-check.js
+++ utility/header-login-check.js
@@ -1,10 +1,23 @@
 const navList = document.querySelector("nav > ul");
 const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
 
-if (loggedInUser && loggedInUser.is_admin) {
-    const navListItem = document.createElement("li");
-    const adminLink = document.createElement("a");
-    adminLink.textContent = "Admin Portal";
-    adminLink.setAttribute("href", "admin.html");
-    navList.appendChild(adminLink);
+if (loggedInUser) {
+    document.querySelector('a[href="login.html"]').remove();
+    if (loggedInUser.is_admin) {
+        const adminListItem = document.createElement("li");
+        const adminLink = document.createElement("a");
+        adminLink.textContent = "Admin Portal";
+        adminLink.setAttribute("href", "admin.html");
+        adminListItem.appendChild(adminLink);
+        navList.appendChild(adminListItem);
+    }
+    const signOutListItem = document.createElement("li");
+    const signOutLink = document.createElement("a");
+    signOutLink.textContent = "Sign Out";
+    signOutLink.setAttribute("href", "/");
+    signOutLink.addEventListener("click", ()=> {
+        localStorage.clear();
+    });
+    signOutListItem.appendChild(signOutLink);
+    navList.appendChild(signOutListItem);
 }
